702912330698a4c9071f8650350cb36246a3ba79
Nadya <nadinru92@mail.ru>
2022-07-01 23:41:49 +0300

    add Example005_if_else

---
+++ Example/Example005_if_else/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите Ваше имя");
+String username = Console.ReadLine();
+
+if (username =="Маша"){
+    Console.WriteLine ("Маша, привет");
+}
+else{
+   Console.WriteLine ("Привет"); 
+}
